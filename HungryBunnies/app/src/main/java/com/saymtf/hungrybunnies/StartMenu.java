8b1c9e41f9a71f1651a18e1bcbba43f22a6edd16
fent4647 <mfenton@csumb.edu>
2015-03-17 19:58:44 -0700

    created an intent to gameactivity

--- HungryBunnies/app/src/main/java/com/saymtf/hungrybunnies/StartMenu.java
+++ HungryBunnies/app/src/main/java/com/saymtf/hungrybunnies/StartMenu.java
@@ -1,18 +1,40 @@
 package com.saymtf.hungrybunnies;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class StartMenu extends Activity {
+
+    Button playGameButton;
+    Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+
+
+        playGameButton = (Button) findViewById(R.id.playGameButton);
+        //settingsButton = (Button) findViewById(R.id.settingsButton);
+
+        playGameButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), GameActivity.class);
+                    startActivity(intent);
+                }
+             }
+        );
+
+
     }
 
 }
