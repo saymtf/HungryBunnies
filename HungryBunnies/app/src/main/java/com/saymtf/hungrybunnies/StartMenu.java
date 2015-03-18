5cd450f94639c15e19a3efc239bcee326edc53e7
fent4647 <mfenton@csumb.edu>
2015-03-17 18:50:27 -0700

    Removed ActionBar from StartMenu.java and added Fullscreen to Manifest

--- HungryBunnies/app/src/main/java/com/saymtf/hungrybunnies/StartMenu.java
+++ HungryBunnies/app/src/main/java/com/saymtf/hungrybunnies/StartMenu.java
@@ -1,12 +1,13 @@
 package com.saymtf.hungrybunnies;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class StartMenu extends ActionBarActivity {
+public class StartMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
